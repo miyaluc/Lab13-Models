16d87d78afecb287ddfabedb7dd4570b72509250
miyaluc <srlucas3@gmail.com>
2017-09-28 12:31:03 -0700

    controller added

---
+++ lab13_miya/lab13_miya/Data/lab13_miyaContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace lab13_miya.Models
+{
+    public class lab13_miyaContext : DbContext
+    {
+        public lab13_miyaContext (DbContextOptions<lab13_miyaContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<lab13_miya.Models.Register> Register { get; set; }
+    }
+}
---
+++ lab13_miya/lab13_miya/Migrations/20170928190844_Initial.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace lab13_miya.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Register",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    Grade = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Talent = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Register", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Register");
+        }
+    }
+}
